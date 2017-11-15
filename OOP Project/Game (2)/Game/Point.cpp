bc698fe4909709e2dd678c82a18475260c342949
Minhaj Ahmed Moin <hm02896@st.habib.edu.pk>
2017-11-15 23:32:36 +0500

    l chus

--- OOP Project/Game (2)/Game/Point.cpp
+++
@@ -1,7 +0,0 @@
-#include "Point.h"
-using namespace std;
-ostream& operator <<(ostream& stream,const Point& toPrint)
-{
-    stream << "(" << toPrint.x << "," << toPrint.y << ")" << endl;
-    return stream;
-}
--- OOP Project/Readme.txt
+++
@@ -1,1 +0,0 @@
-This folder contains backup files of the project. 
